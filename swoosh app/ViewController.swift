bb55cc89bde2e5d29c3946e896b5df8864a4bda9
FAHAD JAFFERI <fahad@FAHADs-MacBook-Pro.local>
2017-10-22 11:07:54 +0700

    Initial Commit

---
+++ swoosh app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swoosh app
+//
+//  Created by FAHAD JAFFERI on 22/10/17.
+//  Copyright © 2017 FAHAD JAFFERI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
